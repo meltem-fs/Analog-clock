cde66045f2718f4d2deeaf6d3318312638b1dd61
MELTEM <meltemfsanaldi@gmail.com>
2022-08-19 20:33:59 +0300

    changed js

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ function tiktak() {
     let minute = new Date().getMinutes();
     let hour = new Date().getHours();
 
-    hours.style.transform = `rotate(${180 + hour * 30}deg)`;
-    sec.style.transform = `rotate(${180 + second * 6}deg)`;
-    min.style.transform = `rotate(${180 + minute * 6}deg)`;
+    hours.style.transform = `rotate(${hour * 30}deg)`;
+    sec.style.transform = `rotate(${second * 6}deg)`;
+    min.style.transform = `rotate(${minute * 6}deg)`;
 }
 
 setInterval(tiktak, 1000);
